925acab3c4758865105281612831831f0e396f89
MimonN <m.a.nedilsky@gmail.com>
2023-04-01 12:34:01 -0400

    infinite scroll with JS

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
-const apiKey = 'ftGXzMvHT_NgR_x4738KEhlj4AcXhZ97eGLuX1v8RmM';
+const count = 30;
+const apiKey = 'eW5D4XKBu7yfbmuTUJR41O5asopcV1HH6QQpyu2BROI';
 let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttribute(element, attributes) {
@@ -17,6 +29,8 @@ function setAttribute(element, attributes) {
 
 // Create Eelements For Links & Photos, Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -35,6 +49,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Elemnent
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -50,6 +66,18 @@ async function getPhotos() {
   } catch (error) {
     // Catch error here
   }
+
+  // Check to see if scrolling near bottom of page, Load More Photos
+  window.addEventListener('scroll', () => {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 1000 &&
+      ready
+    ) {
+      ready = false;
+      getPhotos();
+    }
+  });
 }
 
 // On Load
